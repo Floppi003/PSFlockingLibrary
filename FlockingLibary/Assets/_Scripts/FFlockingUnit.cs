b0aac9f6ebd90ce721591e0ecf3efbc56a3e41a0
Florian Peinsold <f.peinsold@gmail.com>
2017-08-28 11:50:27 +0200

    - Code Cleanup

--- FlockingLibary/Assets/_Scripts/FFlockingUnit.cs
+++ FlockingLibary/Assets/_Scripts/FFlockingUnit.cs
@@ -4,46 +4,49 @@ using UnityEngine;
 
 public class FFlockingUnit : MonoBehaviour {
 
+	// Reference to the Manager Object which has the FUnitManager Script attached to it
 	public GameObject manager;
-	public Vector3 location = Vector3.zero;
-	public Vector3 velocity;
-	Vector3 goalPos = Vector3.zero;
-	Vector3 currentForce;
+
+	private Vector3 velocity;
 	private Vector3 previousPosition = Vector3.zero;
 
+	// For Developers only: change position of goal randomly
 	private float timeUntilNextRandom = 2.0f;
 	private float maximumTimeUntilNextRandom = 3.0f;
 	private float strengthRandomizer;
 
+
+
+	#region MonoBehaviour Subclassing
+
 	void Start() {
+		// set initial values for velocity
 		velocity = new Vector3(Random.Range(0.01f, 0.01f),0f, Random.Range(0.01f, 0.01f));
-		location = new Vector3 (this.gameObject.transform.position.x, this.gameObject.transform.position.y, this.gameObject.transform.position.z);
 	}
 
-	Vector3 seek(Vector3 target) {
-		return(target - location);
-	}
+	void Update() {
+		flock ();
 
-	void applyForce(Vector3 force) {
-		Vector3 appylingForce = new Vector3 (force.x, 0f, force.z);
+		// look to the front
+		this.transform.LookAt(this.transform.position + (this.transform.position - this.previousPosition));
+		this.previousPosition = this.transform.position;
 
-		if (force.magnitude > manager.GetComponent<FUnitManager> ().maxForce) {
-			force = force.normalized;
-			force *= manager.GetComponent<FUnitManager> ().maxForce;
+		// update randomizer strength timer
+		this.timeUntilNextRandom -= Time.deltaTime;
+		if (this.timeUntilNextRandom < 0.0f) {
+			this.makeNewRandom ();
+			this.timeUntilNextRandom = Random.Range (1.0f, this.maximumTimeUntilNextRandom);
 		}
-
-		this.GetComponent<Rigidbody> ().AddForce (appylingForce);
-
-
-		if (this.GetComponent<Rigidbody> ().velocity.magnitude > manager.GetComponent<FUnitManager> ().maxvelocity) {
-			this.GetComponent<Rigidbody> ().velocity = this.GetComponent<Rigidbody> ().velocity.normalized;
-			this.GetComponent<Rigidbody> ().velocity *= manager.GetComponent<FUnitManager> ().maxvelocity;
-		}
-
-		Debug.DrawRay (this.transform.position, appylingForce, Color.red);
 	}
 
-	Vector3 align() {
+	#endregion
+
+
+
+
+	#region Flocking Behaviour
+
+	private Vector3 align() {
 		float alignmentDistance = manager.GetComponent<FUnitManager> ().alignmentDistance;
 
 		Vector3 sum = Vector3.zero;
@@ -54,7 +57,7 @@ public class FFlockingUnit : MonoBehaviour {
 			}
 
 			// check if other boid is within distance 
-			float distance = Vector3.Distance (location, other.GetComponent<FFlockingUnit> ().location);
+			float distance = Vector3.Distance (this.transform.position, other.transform.position);
 			if (distance < alignmentDistance) {
 
 				// check if other boid is within viewing angle
@@ -64,7 +67,7 @@ public class FFlockingUnit : MonoBehaviour {
 				}
 			}
 		}
-			
+
 		if (count > 0) {
 			sum /= count;
 			float strengthMultiplier = this.manager.GetComponent<FUnitManager> ().alignmentStrength + strengthRandomizer;
@@ -77,7 +80,7 @@ public class FFlockingUnit : MonoBehaviour {
 		return Vector3.zero;
 	}
 
-	Vector3 cohesion() {
+	private Vector3 cohesion() {
 		// get the maximum distance other boids can be away to be still taken into account for cohesion
 		float cohesionDistance = manager.GetComponent<FUnitManager> ().cohesionDistance;
 
@@ -94,13 +97,13 @@ public class FFlockingUnit : MonoBehaviour {
 			}
 
 			// get distance from this boid to other boid
-			float distance = Vector3.Distance (location, other.GetComponent<FFlockingUnit> ().location);
+			float distance = Vector3.Distance (this.transform.position, other.transform.position);
 
 			// compare if boid is within distance
 			if (distance < cohesionDistance) {
 				// check if other boid is within viewing angle
 				if (this.isWithinViewingAngle(other)) {
-					sum += other.GetComponent<FFlockingUnit> ().location;
+					sum += other.transform.position;
 					count++;
 				}
 			}
@@ -108,7 +111,7 @@ public class FFlockingUnit : MonoBehaviour {
 
 		if (count > 0) {
 			sum /= count;
-			Vector3 vectorToMiddle = seek (sum);
+			Vector3 vectorToMiddle = this.vectorToTarget (sum);
 			float strengthMultiplier = manager.GetComponent<FUnitManager> ().cohesionStrength + strengthRandomizer;
 			strengthMultiplier = Mathf.Max (strengthMultiplier, 0.0f);
 			strengthMultiplier = Mathf.Min (strengthMultiplier, 1.0f);
@@ -119,7 +122,7 @@ public class FFlockingUnit : MonoBehaviour {
 		return Vector3.zero;
 	}
 
-	Vector3 separation() {
+	private Vector3 separation() {
 		Vector3 force = Vector3.zero;
 		foreach (GameObject other in manager.GetComponent<FUnitManager>().units) {
 			if (other == this.gameObject) {
@@ -127,7 +130,7 @@ public class FFlockingUnit : MonoBehaviour {
 			}
 
 			// get the distance to the other object, and the total allowed distance for seperation to work
-			float distance = Vector3.Distance (location, other.GetComponent<FFlockingUnit>().location);
+			float distance = Vector3.Distance (this.transform.position, other.transform.position);
 			float separationDistance = this.manager.GetComponent<FUnitManager> ().separationDistance;
 
 			// check if the other unit is within the separation-visibility-distance
@@ -135,10 +138,10 @@ public class FFlockingUnit : MonoBehaviour {
 
 				// check if the boid is within viewing angle
 				if (this.isWithinViewingAngle(other)) {
-				
+
 					// bring the force in a range of 0..1, depending on distance
 					float separationForce = Mathf.Pow(1 - (distance / separationDistance), 3) * 10.0f; // makes it exponentially strong when they get really close together
-					Vector3 direction = other.GetComponent<FFlockingUnit> ().location - location;
+					Vector3 direction = other.transform.position - this.transform.position;
 					direction = direction * (-1);
 					direction = direction.normalized;
 					direction = direction * separationForce;
@@ -153,15 +156,14 @@ public class FFlockingUnit : MonoBehaviour {
 		force = force * strengthMultiplier * 2.0f;
 		force = force + force.normalized;
 
-		Debug.Log ("separation force: " + force);
-		Debug.DrawRay (this.transform.position, force, Color.magenta);
+		//Debug.DrawRay (this.transform.position, force, Color.magenta);
 
 		return force;
 	}
 
-	void flock() {
-		location = this.transform.position;
+	private void flock() {
 		velocity = this.GetComponent<Rigidbody> ().velocity;
+		Vector3 currentForce = Vector3.zero;
 
 		if (manager.GetComponent<FUnitManager> ().obedient/* && Random.Range (0, 50) <= 1*/) {
 
@@ -179,7 +181,7 @@ public class FFlockingUnit : MonoBehaviour {
 				// get the goal
 				GameObject goalGO = manager.GetComponent<FUnitManager>().goal;
 				if (goalGO != null) {
-					goal = seek (goalGO.transform.position);
+					goal = this.vectorToTarget (goalGO.transform.position);
 				}
 			}
 
@@ -197,34 +199,50 @@ public class FFlockingUnit : MonoBehaviour {
 		applyForce (currentForce);
 	}
 
+	#endregion
+
+
+
+
+	#region Other Functions
+
+	private Vector3 vectorToTarget(Vector3 target) {
+		return(target - this.transform.position);
+	}
+
+	private void applyForce(Vector3 force) {
+		Vector3 appylingForce = new Vector3 (force.x, 0f, force.z);
+
+		if (force.magnitude > manager.GetComponent<FUnitManager> ().maxForce) {
+			force = force.normalized;
+			force *= manager.GetComponent<FUnitManager> ().maxForce;
+		}
+
+		this.GetComponent<Rigidbody> ().AddForce (appylingForce);
+
+
+		if (this.GetComponent<Rigidbody> ().velocity.magnitude > manager.GetComponent<FUnitManager> ().maxvelocity) {
+			this.GetComponent<Rigidbody> ().velocity = this.GetComponent<Rigidbody> ().velocity.normalized;
+			this.GetComponent<Rigidbody> ().velocity *= manager.GetComponent<FUnitManager> ().maxvelocity;
+		}
+
+		//Debug.DrawRay (this.transform.position, appylingForce, Color.red);
+	}
+
 	private void makeNewRandom() {
 		this.strengthRandomizer = Random.Range (0.0f, manager.GetComponent<FUnitManager> ().randomizerStrength) - (manager.GetComponent<FUnitManager> ().randomizerStrength / 2.0f);
 	}
 
-	void Update() {
-		flock ();
-		goalPos = manager.transform.position;
-
-		// look to the front
-		this.transform.LookAt(this.transform.position + (this.transform.position - this.previousPosition));
-		this.previousPosition = this.transform.position;
-
-		// update strength timer
-		this.timeUntilNextRandom -= Time.deltaTime;
-		if (this.timeUntilNextRandom < 0.0f) {
-			this.makeNewRandom ();
-			this.timeUntilNextRandom = Random.Range (1.0f, this.maximumTimeUntilNextRandom);
-		}
-	}
-
-	bool isWithinViewingAngle(GameObject other) {
+	private bool isWithinViewingAngle(GameObject other) {
 		// check whether that boid is within the viewing angle
 		float viewingAngle = manager.GetComponent<FUnitManager>().viewingAngle;
-		float angle = Vector3.Angle (this.transform.forward, this.seek(other.transform.position).normalized);
+		float angle = Vector3.Angle (this.transform.forward, this.vectorToTarget(other.transform.position).normalized);
 		if (angle < (viewingAngle / 2.0f)) {
 			return true;
 		} else {
 			return false;
 		}
 	}
+
+	#endregion
 }
--- FlockingLibary/Assets/_Scripts/FUnitManager.cs
+++ FlockingLibary/Assets/_Scripts/FUnitManager.cs
@@ -15,35 +15,47 @@ public class FUnitManager : MonoBehaviour {
 	public bool obedient = true;
 	public bool willful = false;
 
+
+	// Force & Velocity
 	[Range(0,10)]
 	public float maxForce = 4.0f;
 
 	[Range(0,5)]
 	public float maxvelocity = 2.0f;
 
+
+	// Alignemnt
 	[Range(0, 1)]
 	public float alignmentStrength = 0.5f;
 
 	[Range(0, 25)]
 	public float alignmentDistance = 6;
 
+
+	// Cohesion
 	[Range(0, 1)]
 	public float cohesionStrength = 0.5f;
 
 	[Range(0, 25)]
 	public float cohesionDistance = 6;
 
+
+	// Separation
 	[Range(0, 1)]
 	public float separationStrength = 0.5f;
 
 	[Range(0, 25)]
 	public float separationDistance = 5;
 
+
+	// Others
 	[Range(0, 1)]
 	public float randomizerStrength = 0.2f;
 
 	[Range(0, 360)]
 	public float viewingAngle = 170.0f;
+
+
 
 	// Update is called once per frame
 	void Start () {
